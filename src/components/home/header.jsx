65dc0e84a239de3d0eea2a10d92f1cbbf1848c31
NavinPanthi <panthinabin341@gmail.com>
2024-05-02 14:57:33 +0545

    Add header component

---
+++ src/components/home/header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="bg-bg-header shadow-lg text-text-color">
+      <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <h1 className="text-3xl font-bold tracking-tight ">MilanSathi</h1>
+      </div>
+    </header>
+  );
+}
